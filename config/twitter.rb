cab72135bdae9b328c000cd94106476dda6e237d
ThePonti <cjponti@gmail.com>
2013-06-17 10:03:56 +0100

    updated twitter.rb

--- config/twitter.rb
+++ config/twitter.rb
@@ -3,14 +3,14 @@ require 'oauth'
 require './config/twitter'
 
 Twitter.configure do |config|
-  config.consumer_key = 'JiGenoMMQZR0NDCaM3gCcw'
-  config.consumer_secret = 'p5nd4l4UMrr0X1rsKlhyaiEB0h6vCE7ujM8MyZ7hY'
-  config.oauth_token = '966006937-GScmtT3FKBbC5FL64B3p6VjIJieKXcxxLImjwcnY'
-  config.oauth_token_secret = '9tei1DuM4UVvlMeDgRxEJUJxKZGJEJfZrXe5XrUJLI'
+  config.consumer_key = 'YOUR_CONSUMER_KEY'
+  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+  config.oauth_token = 'YOUR_OAUTH_TOKEN'
+  config.oauth_token_secret = 'YOUR_OAUTH_SECRET'
 end
 
 def prepare_access_token(oauth_token, oauth_token_secret)
-  consumer = OAuth::Consumer.new("JiGenoMMQZR0NDCaM3gCcw", "p5nd4l4UMrr0X1rsKlhyaiEB0h6vCE7ujM8MyZ7hY",
+  consumer = OAuth::Consumer.new("YOUR_CONSUMER_KEY", "YOUR_CONSUMER_SECRET",
     { :site => "http://api.twitter.com",
       :scheme => :header
     })
@@ -21,9 +21,9 @@ def prepare_access_token(oauth_token, oauth_token_secret)
   access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
   return access_token
 end
- 
+
 # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-access_token = prepare_access_token("966006937-GScmtT3FKBbC5FL64B3p6VjIJieKXcxxLImjwcnY", "9tei1DuM4UVvlMeDgRxEJUJxKZGJEJfZrXe5XrUJLI")
+access_token = prepare_access_token("YOUR_OAUTH_TOKEN", "YOUR_OAUTH_SECRET")
 
 # use the access token as an agent to get the home timeline
 response = access_token.request(:get, "https://api.twitter.com/1.1/statuses/home_timeline.json")
